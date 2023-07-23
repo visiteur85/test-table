f1f6f6dfb03ecbc275c4dcc19f5e65972cfac845
admin <krivalcevich@gmail.com>
2023-07-23 19:43:38 +0300

    bugfix with page in url

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import MainPage from "./Pages/MainPage/MainPage";
-import {getUsersTS} from "./store/usersReducer";
+import {getUsersTS, setCurrentPageAC} from "./store/usersReducer";
 import {useAppDispatch} from "./store/store";
 
 function App() {
     const dispatch = useAppDispatch()
 
     useEffect(() => {
+        const currentUrl = window.location.href;
+        const url = new URL(currentUrl);
+        const searchParams = new URLSearchParams(url.search);
+        const pageNumber = searchParams.get('page');
+        if (pageNumber) {
+            dispatch(setCurrentPageAC(+pageNumber))
+        }
+
         dispatch(getUsersTS())
 
     }, [])
